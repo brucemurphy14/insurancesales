54e438c96788d5f19fdadb32f2acab5a8689b46a
Ryan <ryanwoodford1@hotmail.com>
2021-03-03 21:32:35 -0330

    Added rudimentary security on /client URL for testing.

---
+++ src/main/java/com/insuranceproject/insurancesales/SecurityConfiguration/SecurityConfiguration.java
@@ -0,0 +1,58 @@
+package com.insuranceproject.insurancesales.SecurityConfiguration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+
+    /**
+     * Connects to datasource and retrieves usernames/password from users table.
+     * @param auth
+     * @throws Exception
+     */
+    @Autowired
+    protected void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from users where username=?")
+                .authoritiesByUsernameQuery("select username, role from users where username=?");
+
+    }
+
+    /**
+     * authenticates login information inputted from the form. Triggers when the /client URL is navigated to and requires a valid log in.
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected  void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/client").hasRole("CLIENT")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+    }
+
+    /**
+     * The most simple password encryption. I.E none at all.
+     * @return
+     */
+    //TODO implement some sort of password hashing.
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
--- src/main/java/com/insuranceproject/insurancesales/dao/DAO.java
+++ src/main/java/com/insuranceproject/insurancesales/dao/DAO.java
@@ -7,6 +7,7 @@ import java.util.Optional;
  * This interface shows the required behaviour for the objects in the relational model.
  */
 public interface DAO<T> {
+
     List<T> list() ;
     void create(T t);
     Optional<T> get(int id);
